d84929517e61d80b77938d6d6d7ce8643ee3b312
dddev965 <dddev965@gmail.com>
2024-04-15 14:29:49 +0530

    adding expresss

---
+++ server.js
@@ -0,0 +1,9 @@
+// import http from "http";
+const http = require("http")
+
+
+const server = http.createServer((req,res)=>{
+    // console.log(req.url,req.method,req.headers);
+});
+
+server.listen(3000)
